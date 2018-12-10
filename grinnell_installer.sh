0e340481af4170cc3fff926a6cce95054b516ceb
Mark McFate <mcfatem@grinnell.edu>
2018-12-10 13:54:03 -0600

    Commented out problems.

--- grinnell_installer.sh
+++ grinnell_installer.sh
@@ -150,14 +150,14 @@ drush iabookreader-plugin
 
 # MAM addtions for DG-specific Drupal modules.  See drupal.drush.make
 drush -y -u 1 en announcements
-drush -y -u 1 en digital_grinnell_theme
+# drush -y -u 1 en digital_grinnell_theme
 drush -y -u 1 en dg7
 drush -y -u 1 en email
 drush -y -u 1 en google_analytics_counter
-drush -y -u 1 en google_analytics_report
-drush -y -u 1 en google_analytics_report_api
+# drush -y -u 1 en google_analytics_report
+# drush -y -u 1 en google_analytics_report_api
 drush -y -u 1 en imagemagick_advanced
-drush -y -u 1 en jw_player
+# drush -y -u 1 en jw_player
 drush -y -u 1 en ldap_authentication
 drush -y -u 1 en ldap_authorization_drupal_role
 drush -y -u 1 en ldap_authorization
@@ -168,7 +168,7 @@ drush -y -u 1 en ldap_test
 drush -y -u 1 en ldap_user
 drush -y -u 1 en maillog
 drush -y -u 1 en masquerade
-drush -y -u 1 en phpmailer
+# drush -y -u 1 en phpmailer
 drush -y -u 1 en r4032login
 drush -y -u 1 en views_bootstrap
 
@@ -177,9 +177,9 @@ drush -y -u 1 en islandora_binary_object
 drush -y -u 1 en islandora_collection_search
 # DEPRECATED drush -y -u 1 en islandora_jw_player
 drush -y -u 1 en islandora_mods_display
-drush -y -u 1 en islandora_pdfjs_reader
+# drush -y -u 1 en islandora_pdfjs_reader
 drush -y -u 1 en islandora_solr_collection_view
-drush -y -u 1 en islandora_solution_pack_oralhistories
+# drush -y -u 1 en islandora_solution_pack_oralhistories
 
 # Due to Islandora Paged Content Module install hook, the islandora_paged_content_gs variable is overwritten by the install / enabling of the module back to /usr/bin/gs
 echo "Rerunning drush vset to ensure that Ghostscript works for the PDF DERIVATIVE SETTINGS"
