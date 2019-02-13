00cff0ba45c2222c6bc25acf331f76dc8113ec03
SummittDweller <summittdweller@gmail.com>
2019-02-13 15:57:11 -0600

    Commit from MA8660

--- grinnell_installer.sh
+++ grinnell_installer.sh
@@ -22,7 +22,7 @@ drush make --prepare-install /utility-scripts/isle_drupal_build_tools/isle-drush
 
 echo "Using Islandora makefile for Islandora modules for sample Drupal site within /tmp/drupal_install"
 # drush make --no-core /utility-scripts/isle_drupal_build_tools/isle-drush_make/islandora.drush.make /tmp/drupal_install
-drush make --no-core /utility-scripts/isle_drupal_build_tools/isle-drush_make/isle.islandora.make.yml /tmp/drupal_install
+drush --verbose make --no-core /utility-scripts/isle_drupal_build_tools/isle-drush_make/isle.islandora.make.yml /tmp/drupal_install
 
 # @TODO pass by var
 echo "Update settings.php with ISLE default"
