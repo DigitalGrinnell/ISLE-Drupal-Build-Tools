59582b46925567c7c366264e315a15960dbca7f8
SummittDweller <summittdweller@gmail.com>
2019-03-12 15:57:41 -0500

    Enabling islandora_pdfjs_reader

--- custom.d/drush-enable-modules.sh
+++ custom.d/drush-enable-modules.sh
@@ -42,9 +42,9 @@ drush -y -u 1 en islandora_binary_object --skip
 drush -y -u 1 en islandora_collection_search --skip
 # DEPRECATED drush -y -u 1 en islandora_jw_player
 drush -y -u 1 en islandora_mods_display --skip
-# drush -y -u 1 en islandora_pdfjs_reader
+drush -y -u 1 en islandora_pdfjs_reader
 drush -y -u 1 en islandora_solr_collection_view --skip
-# drush -y -u 1 en islandora_solution_pack_oralhistories
+drush -y -u 1 en islandora_solution_pack_oralhistories
 drush -y -u 1 en islandora_xslt_paths --skip
 
 drush -y -u 1 en idu --skip
